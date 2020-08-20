ccb4bad173971336aea4ed982b53ea9c5dbc0b1e
serizawa <yserory@gmail.com>
2020-08-20 19:09:46 +0900

    リロード時の不具合修正

--- src/general/user/login/login.component.ts
+++ src/general/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { User } from '../../../shared/model/user';
+import { UserService } from '../../../shared/api/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder,
+              private user: UserService) { }
 
-  public test: string;
+  public inputData: FormGroup;
   public hide: boolean = true;
 
   ngOnInit(): void {
+    this.inputData = this.formBuilder.group({
+      name: new FormControl('', [ Validators.required ]),
+      password: new FormControl('', [ Validators.required ]),
+    });
+  }
+
+  public onSubmit(): void {
   }
 
 }
