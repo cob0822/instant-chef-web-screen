3d9349e7b06a16113070018a59b6429f2bc960cf
serizawa <yserory@gmail.com>
2020-10-11 12:31:38 +0900

    レシピ注文APIリクエスト　修正

--- src/shared/api/order.service.ts
+++ src/shared/api/order.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/index';
+import { AuthService } from '../service/auth.service'
 import { OrderRequest } from '../model/order-request';
-import { LocalStorageService } from '../service/local-storage.service';
-import { LocalStorageKey } from '../enum/local-storage-key';
 import { RequestWithAccessToken } from '../model/request-with-access-token';
 
 @Injectable({
@@ -12,14 +11,14 @@ import { RequestWithAccessToken } from '../model/request-with-access-token';
 export class OrderService {
 
   constructor(protected http: HttpClient,
-              private localStorage: LocalStorageService) { }
+              private auth: AuthService) { }
 
   public searchCategory(keyword: string): Observable<{id: number, name: string}[]> {
     return this.http.get<{id: number, name: string}[]>('/api/orders/categories', {params: new HttpParams().set('keyword', keyword)});
   }
 
   public createOrder(orderRequest: OrderRequest): Observable<OrderRequest> {
-    let request: OrderRequest & RequestWithAccessToken = Object.assign({}, orderRequest, {access_token: this.localStorage.getLocalStorage(LocalStorageKey.AccessToken)});
+    let request: OrderRequest & RequestWithAccessToken = Object.assign({}, orderRequest, {access_token: this.auth.accessToken});
     return this.http.post<OrderRequest>('/api/orders', {request: request});
   }
 }
